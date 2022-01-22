34622b515e359f9eaa7641e2b8ae365bfbd53924
Alex Seifert <alex@alexseifert.com>
2022-01-22 23:28:06 +0100

    Move the config to its own file to avoid conflict between the main and renderer processes

--- src/i18n/index.ts
+++ src/i18n/index.ts
@@ -1,24 +1,13 @@
-import i18n, { InitOptions } from 'i18next';
+import i18n from 'i18next';
 import { initReactI18next } from 'react-i18next';
 
-import config from '../config';
-import resources from './locales';
-
-export const defaultConfig: InitOptions = {
-  resources,
-  defaultNS: 'common',
-  fallbackLng: config.intl.defaultLocale,
-  supportedLngs: config.intl.languages,
-  preload: config.intl.languages,
-  keySeparator: false,
-  debug: process.env.NODE_ENV === 'development',
-};
+import defaultConfig from './config';
 
 i18n
   .use(initReactI18next)
   .init({
     ...defaultConfig,
-    lng: window.app.getLocale(),
+    lng: (window as any).app.getLocale(),
   });
 
 export default i18n;
--- src/i18n/main.ts
+++ src/i18n/main.ts
@@ -2,7 +2,7 @@ import { app } from 'electron';
 import i18n from 'i18next';
 import { initReactI18next } from 'react-i18next';
 
-import { defaultConfig } from './index';
+import defaultConfig from './config';
 
 i18n
   .use(initReactI18next)
